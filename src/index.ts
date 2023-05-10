4b0cb8613b3c4d92568cf2abc98d81fc754e3215
Augustine Smith <augustine.smith@amalitech.org>
2023-05-10 15:20:33 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import core from '@actions/core';
 import github from '@actions/github';
 
 async function run() {
-
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    console.log("Running");
 }
 
 run();
