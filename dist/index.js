26c6fe169685fa18ae00f20c40f8cee3b81f4452
Augustine Smith <augustine.smith@amalitech.org>
2023-05-10 15:57:44 +0000

    update: different imports

--- dist/index.js
+++ dist/index.js
@@ -9617,6 +9617,29 @@ function wrappy (fn, cb) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9626,27 +9649,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
-const github_1 = __importDefault(__nccwpck_require__(5438));
+const core = __importStar(__nccwpck_require__(2186));
+const github = __importStar(__nccwpck_require__(5438));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const GITHUB_TOKEN = core_1.default.getInput("GITHUB_TOKEN");
-            const octokit = github_1.default.getOctokit(GITHUB_TOKEN);
+            const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+            const octokit = github.getOctokit(GITHUB_TOKEN);
             const sha = process.env.GITHUB_SHA;
             const message = process.env.GITHUB_MESSAGE;
             const author = process.env.GITHUB_ACTOR;
             const commitInfo = `Commit: ${sha}\nAuthor: ${author}\nMessage: ${message}\n`;
             console.log(commitInfo);
-            core_1.default.setOutput("commit-info", commitInfo);
+            core.setOutput("commit-info", commitInfo);
         }
         catch (error) {
             if (error instanceof Error) {
-                core_1.default.setFailed(error.message);
+                core.setFailed(error.message);
             }
         }
     });
