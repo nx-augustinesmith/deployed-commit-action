94cc70f13388f3a2fbb045acb6725e98c0afdc60
Augustine Smith <augustine.smith@amalitech.org>
2023-05-11 08:50:06 +0000

    using a nested path

--- src/index.ts
+++ src/index.ts
@@ -26,57 +26,12 @@ async function run() {
     await octokit.createOrUpdateTextFile({
       owner: "nx-augustinesmith",
       repo: "deployed-commit-action",
-      path: "COMMIT_INFO",
+      path: "src/COMMIT_INFO",
       message: "Updated COMMIT_INFO",
       content: ({ content }) => {
         return commitInfo;
       },
     });
-    
-    // fs.writeFileSync("COMMIT_INFO", commitInfo);
-    // const repo = github.context.repo;
-    // const branch = github.context.ref.replace("refs/heads/", "");
-    // const blob = await octokit.rest.git.createBlob({
-    //   owner: repo.owner,
-    //   repo: repo.repo,
-    //   content: commitInfo,
-    //   encoding: "utf-8",
-    // });
-    // const tree = await octokit.rest.git.getTree({
-    //   owner: repo.owner,
-    //   repo: repo.repo,
-    //   tree_sha: `${sha}^{tree}`,
-    // });
-
-    // const newTree = await octokit.rest.git.createTree({
-    //   owner: repo.owner,
-    //   repo: repo.repo,
-    //   tree: tree.data.tree.concat({
-    //     path: "COMMIT_INFO",
-    //     mode: "100644",
-    //     type: "blob",
-    //     sha: `${sha}^{blob}`,
-    //   }),
-    // } as any);
-    // const newCommit = await octokit.rest.git.createCommit({
-    //   owner: repo.owner,
-    //   repo: repo.repo,
-    //   message: "Add COMMIT_INFO",
-    //   tree: newTree.data.sha,
-    //   parents: [sha],
-    //   author: {
-    //     name: author,
-    //     email: "nx-augustinesmith@nexum.com",
-    //     date: new Date().toISOString(),
-    //   },
-    // });
-
-    // await octokit.rest.git.updateRef({
-    //   owner: repo.owner,
-    //   repo: repo.repo,
-    //   ref: `heads/${branch}`,
-    //   sha: newCommit.data.sha,
-    // });
     core.setOutput("commit-info", commitInfo);
   } catch (error) {
     if (error instanceof Error) {
